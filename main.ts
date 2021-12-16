8cbf7832d24f0343281f5245dc91635b246d2efd
Jeff <jeff@cusk.io>
2021-12-16 14:32:20 -0700

    fixed arrow directions and using degrees variable

--- main.ts
+++ main.ts
@@ -1,20 +1,23 @@
+let degrees = 0
 soundExpression.hello.playUntilDone()
+input.calibrateCompass()
 basic.forever(function () {
-    if (input.compassHeading() < 23 || input.compassHeading() >= 337) {
+    degrees = input.compassHeading()
+    if (degrees < 23 || degrees >= 337) {
         images.arrowImage(ArrowNames.North).showImage(0)
-    } else if (input.compassHeading() < 67) {
-        images.arrowImage(ArrowNames.NorthEast).showImage(0)
-    } else if (input.compassHeading() < 112) {
-        images.arrowImage(ArrowNames.East).showImage(0)
-    } else if (input.compassHeading() < 157) {
-        images.arrowImage(ArrowNames.SouthEast).showImage(0)
-    } else if (input.compassHeading() < 202) {
-        images.arrowImage(ArrowNames.South).showImage(0)
-    } else if (input.compassHeading() < 247) {
-        images.arrowImage(ArrowNames.SouthWest).showImage(0)
-    } else if (input.compassHeading() < 292) {
-        images.arrowImage(ArrowNames.West).showImage(0)
-    } else {
+    } else if (degrees < 67) {
         images.arrowImage(ArrowNames.NorthWest).showImage(0)
+    } else if (degrees < 112) {
+        images.arrowImage(ArrowNames.West).showImage(0)
+    } else if (degrees < 157) {
+        images.arrowImage(ArrowNames.SouthWest).showImage(0)
+    } else if (degrees < 202) {
+        images.arrowImage(ArrowNames.South).showImage(0)
+    } else if (degrees < 247) {
+        images.arrowImage(ArrowNames.SouthEast).showImage(0)
+    } else if (degrees < 292) {
+        images.arrowImage(ArrowNames.East).showImage(0)
+    } else {
+        images.arrowImage(ArrowNames.NorthEast).showImage(0)
     }
 })
